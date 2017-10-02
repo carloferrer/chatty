3122fb30a695af00693ed7a89ac2577dbc2cbb5e
vagrant <thecarloferrer@gmail.com>
2017-10-02 21:26:03 +0000

    Completed visualization.

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,9 @@ console.log('App.jsx > import react and whatnot');
 import React, {Component} from 'react';
 
 class App extends Component {
-  console.log('App.jsx > inside class definition');
   render() {
     console.log('App.jsx > inside render');
     return (
-      console.log('App.jsx > inside return');
       <h1>Hello React :)</h1>
     );
   }
