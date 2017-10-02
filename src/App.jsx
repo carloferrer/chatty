37b9e7376495409808092bab286b5a3dd805b1f6
vagrant <thecarloferrer@gmail.com>
2017-10-02 21:04:15 +0000

    Completed implementation of many console.logs.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+console.log('App.jsx > import react and whatnot');
 import React, {Component} from 'react';
 
 class App extends Component {
+  console.log('App.jsx > inside class definition');
   render() {
+    console.log('App.jsx > inside render');
     return (
+      console.log('App.jsx > inside return');
       <h1>Hello React :)</h1>
     );
   }
